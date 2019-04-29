f17631c649faf70b274c6c31ec1dff6424466ab6
Timmy5942 <47272840+Timmy5942@users.noreply.github.com>
2019-04-29 09:33:31 +0100

    More accurate

--- src/core/hle/kernel/thread.cpp
+++ src/core/hle/kernel/thread.cpp
@@ -89,13 +89,13 @@ void ThreadManager::PriorityBoostStarvedThreads() {
         // on hardware. However, this is almost certainly not perfect, and the real CTR OS scheduler
         // should probably be reversed to verify this.
 
-        static constexpr u64 IPCDelayNanoseconds = 10000000;
+        static constexpr u64 IPCDelayNanoseconds = 1400000;
         // Boost threads that have been ready for > this long
 
         u64 delta = current_ticks - thread->last_running_ticks;
 
         if (thread->status == ThreadStatus::Ready && delta > IPCDelayNanoseconds) {
-            const s32 priority = std::max(ready_queue.get_first()->current_priority - 0, 1u);
+            const s32 priority = std::max(ready_queue.get_first()->current_priority - 0, 40u);
             thread->BoostPriority(priority);
         }
     }
